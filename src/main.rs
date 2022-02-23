5ac115027f0ac519f5d522cf3f147cace1447aee
Amr Elbeleidy <hi@amr.dev>
2022-02-23 20:26:13 +0000

    add DO database

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -14,8 +12,8 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string().expose_secret())
-        .expect("Failed to connect to postgres");
+        .connect_lazy_with(configuration.database.with_db());
+
     let port = configuration.application.port;
     let host = configuration.application.host;
     let address = format!("{host}:{port}");
